61d70b21322f85a70506aa0fd053bbbb53a1f7f8
Pooja <pooja.pimpalshende215@gmail.com>
2025-03-17 15:19:22 +0100

    Hero section content with primary styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,36 @@
+import "./Home.css";
+import heroImg from "../assets/img/hero.png";
+
 const Home = () => {
   return (
     <>
-      <h1>A healthy meal delivered to your door, every single day</h1>
+      <section className="section-hero">
+        <div className="hero">
+          <div className="hero-text-box">
+            <h1 className="heading-primary">
+              A healthy meal delivered to your door, every single day
+            </h1>
+            <p className="hero-description">
+              The smart 365-days-per-year food subscription that will make you
+              eat healthy again. Tailored to your personal tastes and
+              nutritional needs. We have delivered 250,000+ meals last year!
+            </p>
+            <a href="#" className="btn">
+              Start eating well
+            </a>
+            <a href="a" className="btn">
+              Learn more &darr;
+            </a>
+          </div>
+          <div className="hero-img-box">
+            <img
+              src={heroImg}
+              className="hero-img"
+              alt="Women enjoying food, meals in storage container and food bowls on a table"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
