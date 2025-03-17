ba30daad54a78fa7e2c4e051eee3db187f7c2dd7
Pooja <pooja.pimpalshende215@gmail.com>
2025-03-17 17:45:32 +0100

    Global font style-Rubik,Implement dynamic CustomerGallery with import.meta.glob(). Styling of customer gallery.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,28 @@
 import "./Home.css";
 import heroImg from "../assets/img/hero.png";
+import { useEffect, useState } from "react";
+
+const images = import.meta.glob("../assets/img/customers/customer-*.jpg");
+
+const CustomerGallery = () => {
+  const [imageUrls, setImageUrls] = useState([]);
+
+  useEffect(() => {
+    Promise.all(
+      Object.values(images).map((importFn) =>
+        importFn().then((mod) => mod.default)
+      )
+    ).then(setImageUrls);
+  }, []);
+
+  return (
+    <div className="dilivered-imgs">
+      {imageUrls.map((src, index) => (
+        <img key={index} src={src} alt={`Customer ${index + 1}`} />
+      ))}
+    </div>
+  );
+};
 
 const Home = () => {
   return (
@@ -13,7 +36,7 @@ const Home = () => {
             <p className="hero-description">
               The smart 365-days-per-year food subscription that will make you
               eat healthy again. Tailored to your personal tastes and
-              nutritional needs. We have delivered 250,000+ meals last year!
+              nutritional needs.
             </p>
             <a href="#" className="btn btn--full margin-right-sm">
               Start eating well
@@ -21,6 +44,12 @@ const Home = () => {
             <a href="#" className="btn btn--outline">
               Learn more &darr;
             </a>
+            <div className="dilivered-meals">
+              <CustomerGallery />
+              <p className="delivered-text">
+                <span>250,000+</span> meals delivered last year!
+              </p>
+            </div>
           </div>
           <div className="hero-img-box">
             <img
